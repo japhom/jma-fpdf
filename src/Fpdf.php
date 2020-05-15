d506d9b92e78e5b04abe529b0c608a19ae27d1a5
Japhet <japhetmata@gmail.com>
2020-05-15 15:03:27 -0500

    se modifican los namespaces

--- src/Fpdf.php
+++ src/Fpdf.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Crabbly\Fpdf;
+namespace Japhom\Fpdf;
 
 /*******************************************************************************
 * FPDF                                                                         *
